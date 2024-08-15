ca624c15938547d34d33c919bb49cef335bd5be1
Rafał Prażmo <rafalprazmo@protonmail.com>
2024-08-15 14:00:50 +0200

    add safe parameter

--- automice.py
+++ automice.py
@@ -3,8 +3,8 @@ import json
 from pynput import mouse
 
 class MouseListener:
-    def __init__(self, safe:bool=True):
-        self.safe = safe
+    def __init__(self):
+        self.safe = True
         self.seq = []
         self.delay = 0
         self.start_time = 0
@@ -12,10 +12,13 @@ class MouseListener:
         self.stop_after = None
         self.stop_on = ''
 
-    def on_move(self, x, y):
+    def _on_move(self, x, y):
         t = time.time()
         
         if self.stop_on == 'move' or (self.stop_after and t - self.start_time > self.stop_after):
+            return False
+
+        if self.safe and not self.stop_after and not self.stop_on and t - self.start_time > 10:
             return False
         
         event = {
@@ -28,10 +31,13 @@ class MouseListener:
         self.t = t
         self.seq.append(event)
 
-    def on_click(self, x, y, button, pressed):
+    def _on_click(self, x, y, button, pressed):
         t = time.time()
         
         if self.stop_on == 'click' or (self.stop_after and t - self.start_time > self.stop_after):
+            return False
+        
+        if self.safe and not self.stop_after and not self.stop_on and t - self.start_time > 10:
             return False
         
         b = ''
@@ -55,10 +61,13 @@ class MouseListener:
         self.t = t
         self.seq.append(event)
 
-    def on_scroll(self, x, y, dx, dy):
+    def _on_scroll(self, x, y, dx, dy):
         t = time.time()
         
         if self.stop_on == 'scroll' or (self.stop_after and t - self.start_time > self.stop_after):
+            return False
+        
+        if self.safe and not self.stop_after and not self.stop_on and t - self.start_time > 10:
             return False
         
         event = {
@@ -73,26 +82,27 @@ class MouseListener:
         self.t = t
         self.seq.append(event)
         
-    def listen(self, delay:int=0, stop_after:int=None, stop_on:str=''):
+    def listen(self, delay:int=0, stop_after:int=None, stop_on:str='', safe:bool=True):
         """
         Listen for mouse events.
         
         :param delay: Number of seconds to wait before starting listener.
-        :param stop_after: After listening for this many seconds, any subsequent event will stop the listener.
+        :param stop_after: Return on next event after listening for this many seconds.
         :param stop_on: An event that stops the listener. One of move | click | scroll.
+        :param safe: If False and no stop condition is given, the listener will run forever.
         :return: List of dicts describing each event.
         """
         
-        if stop_after:
-            self.stop_after = stop_after
-
-        if stop_on:
-            self.stop_on = stop_on
+        self.safe = safe
+        self.stop_after = stop_after
+        self.stop_on = stop_on
         
         if delay:
+            print(f'Waiting for {delay} seconds')
             time.sleep(delay)
 
-        with mouse.Listener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll) as listener:
+        print('Starting listener')
+        with mouse.Listener(on_move=self._on_move, on_click=self._on_click, on_scroll=self._on_scroll) as listener:
             self.start_time = time.time()
             self.t = time.time()
             listener.join()
@@ -109,12 +119,13 @@ class MouseListener:
             json.dump(self.seq, f)
 
                 
-def run_mouse_events(fp:str, delay:int=0):
+def run_mouse_events(fp:str, delay:int=0, safe:bool=True):
     """
     Execute a series of mouse events.
     
     :param fp: File path to read from.
-    :param delay: Number of seconds to wait before starting execution.
+    :param delay: Number of seconds to wait before running first event.
+    :param safe: If True, function will return on next iteration after running for 10 seconds. Default is True.
     """
     
     controller = mouse.Controller()
@@ -122,9 +133,17 @@ def run_mouse_events(fp:str, delay:int=0):
     with open(fp, 'r', encoding='utf-8') as f:
         seq = json.load(f)
     
-    time.sleep(delay)
+    if delay:
+        print(f'Waiting for {delay} seconds')
+        time.sleep(delay)
+        
+    start_time = time.time()
     
+    print('Running events')
     for event in seq:
+        if safe and time.time() - start_time > 10:
+            return
+        
         time.sleep(event['delay'])
         controller.position = (event['x'], event['y'])
         
@@ -146,4 +165,3 @@ def run_mouse_events(fp:str, delay:int=0):
                         
             case 'scroll':
                 controller.scroll(event['dx'], event['dy'])
-        
